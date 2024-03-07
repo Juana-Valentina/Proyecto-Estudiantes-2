193922b6908386db9d944739bcf219a7778bd09d
Juana Valentina Gonzalez Marquez <juanasena-2005@outlook.com>
2024-03-07 11:37:05 -0500

    Subida 2

--- P-Estudiante/src/app/app.component.ts
+++ P-Estudiante/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from './modules/material/material.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, MaterialModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'P-Estudiante';
+ 
 }
---
+++ P-Estudiante/src/app/modules/material/material.module.ts
@@ -0,0 +1,135 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Importaciones de Angular Material
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTreeModule } from '@angular/material/tree';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSortModule } from '@angular/material/sort';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatRippleModule } from '@angular/material/core';
+import { LayoutModule } from '@angular/cdk/layout';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatSliderModule,
+  LayoutModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatBottomSheetModule,
+  MatChipsModule,
+  MatButtonToggleModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatProgressBarModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatInputModule,
+  MatSelectModule,
+  MatTreeModule,
+  DragDropModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule,
+  MatFormFieldModule,
+  MatSortModule,
+  MatRadioModule,
+  MatDialogModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatTooltipModule,
+  CdkAccordionModule,
+  MatNativeDateModule,
+  MatSnackBarModule,
+  MatStepperModule,
+  MatTabsModule,
+  MatSlideToggleModule,
+  ScrollingModule,
+  MatAutocompleteModule,
+  MatDividerModule,
+  MatRippleModule
+  ],
+  exports: [
+    // Export the same modules to make them available in other parts of your app
+    MatSliderModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatBottomSheetModule,
+    MatChipsModule,
+    MatButtonToggleModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatProgressBarModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTreeModule,
+    DragDropModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatSortModule,
+    MatRadioModule,
+    MatDialogModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatTooltipModule,
+    CdkAccordionModule,
+    MatNativeDateModule,
+    MatSnackBarModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatSlideToggleModule,
+    ScrollingModule,
+    MatAutocompleteModule,
+    MatDividerModule,
+    MatRippleModule,
+  ],
+})
+
+
+export class MaterialModule { }
