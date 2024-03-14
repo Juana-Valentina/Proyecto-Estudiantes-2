98bf4672d102f08f809e519844f7710eac0d1fa4
Juana Valentina Gonzalez Marquez <juanasena-2005@outlook.com>
2024-03-14 11:51:20 -0500

    subida 6

--- P-Estudiante/src/app/components/components.module.ts
+++ P-Estudiante/src/app/components/components.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    SpinnerComponent
+  ],
+
   imports: [
     CommonModule
-  ]
+  ],
 })
 export class ComponentsModule { }
--- P-Estudiante/src/app/components/navbar/navbar.component.spec.ts
+++ P-Estudiante/src/app/components/navbar/navbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SpinnerComponent } from './spinner.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('SpinnerComponent', () => {
-  let component: SpinnerComponent;
-  let fixture: ComponentFixture<SpinnerComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SpinnerComponent]
+      declarations: [NavbarComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(SpinnerComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
